edcc0f3e0d06de8be3b6084ec6f5fe700ef6ba9e
AntiVIRUZ <zwtdbx@yandex.ru>
2015-04-22 17:33:31 +0300

    Изменены функции парсинга в соответсвии с новым форматом данных. Исправлены ошибки в json файле

--- mpk_client/JSONParser.php
+++ mpk_client/JSONParser.php
@@ -5,12 +5,12 @@ include_once 'FilesLoader.php';
 
 class JSONParser extends ParserAbstract {
     
-    public function ParseFromString($xmlString) {
-        $result = json_decode($xmlString, true);
+    public function ParseFromString($jsonString) {
+        $result = json_decode($jsonString, true);
         if ($result === null) {
             trigger_error("Ошибка, неверно сформирован полученный файл\nJSON имеет синтаксические ошибки", E_USER_ERROR);
         }
-        
+        print_r($result);
         if (!parent::VerifyArray($result)) {
             trigger_error($this->lastError, E_USER_ERROR);
         }
