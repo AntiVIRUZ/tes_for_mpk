bc170f688384b54f1d075f11db3c68c7aaaef690
AntiVIRUZ <zwtdbx@yandex.ru>
2015-04-22 16:53:37 +0300

    Добавлен парсер из представления массива Teams к представлению, пригодному к записи в базу данных

--- mpk_client/DBSaver.php
+++ mpk_client/DBSaver.php
@@ -11,11 +11,9 @@ class DBSaver implements iSaver {
     private $connectionStatus;
     private $dbSettings;
     private $lastStatement;
-    
     private $primaryKeyStartValue = array();
-    
-    private $tables = array( "sports_kinds", "participants", "teams", "participants_teams");
-    
+    private $tables = array( "sports_kinds", "members", "teams", "members_teams");
+
     function __construct() {
         $this->connectionStatus = false;
         try {
@@ -25,17 +23,17 @@ class DBSaver implements iSaver {
             die($e->getMessage());
         }
     }
-    
+
     public function GetConnectionStatus() {
         return $this->connectionStatus;
     }
-    
+
     public function GetLastError() {
         return $this->lastError;
     }
 
     public function SetParticipants($participants) {
-        $this->participants = $participants;
+        
     }
 
     public function Save() {
@@ -43,61 +41,23 @@ class DBSaver implements iSaver {
             $this->SaveTable($table, $this->participants[$table]);
         }
     }
-    
-    private function SaveTable($name, $values) {
-        $params = array();
-        $sql = "INSERT INTO `" . $name . "` (";
-        foreach ($values[0] as $key => $field) {
-            $sql .= $key. ",";
-        }
-        $sql[strlen($sql)-1] = ") ";
-        $sql .= "VALUES";
-        
-        foreach ($values as $field){
-            $sql .= "(";
-            foreach ($field as $value) {
-                $sql .= "?,";
-                array_push($params, $value);
-            }
-            $sql[strlen($sql)-1] = ")";
-            $sql .= ",";
-        }
-        $sql[strlen($sql)-1] = ";";
-        return $this->SentQuery($sql, $params);
-    }
-    
-    private function IncrementIds(&$values, $num) {
-        foreach ($values as $key => $value) {
-            $values[$key]["id"] += $num;
-        }
-        echo "num = ".$num;
-    }
-    
-    private function GetMaxIdFromTable($name) {
-        $result = $this->SentQuery("SELECT MAX(id) FROM ". $name);
-        if ($result) {
-            $info = $this->lastStatement->fetch();
-            return $info["MAX(id)"];
-        }
-    }
-    
-    public function ConnectToDB () {
+
+    public function ConnectToDB() {
         return $this->ConnectToSpecificBD($this->dbSettings->getDbType(), $this->dbSettings->getServername(), $this->dbSettings->getUsername(), $this->dbSettings->getPassword(), $this->dbSettings->getDatabase());
     }
-    
-    public function ConnectToSpecificBD($dbType, $servername, $username, $password, $database)
-    {
+
+    public function ConnectToSpecificBD($dbType, $servername, $username, $password, $database) {
         if (!$this->checkSupportingDBDriver($this->dbSettings->getDbType())) {
             trigger_error("Драйвер базы данных не поддерживается или указан неверно", E_USER_ERROR);
         }
-        
+
         if ($this->connectionStatus) {
             $this->DisconnectFromDB();
         }
-        
+
         $connectionString = $this->dbSettings->getDbType() . ":";
         $connectionString .= "host=" . $servername;
-        
+
         try {
             $this->pdo = new PDO($connectionString, $username, $password);
         } catch (Exception $e) {
@@ -105,8 +65,8 @@ class DBSaver implements iSaver {
             $this->lastError = $e->getMessage();
             return FAlSE;
         }
-        
-        if ( $this->CreateDatabaseIfNotExists($database) ) {
+
+        if ($this->CreateDatabaseIfNotExists($database)) {
             $this->SelectDatabase($database);
             $this->DeleteTables();
             $this->CreateTables();
@@ -116,11 +76,99 @@ class DBSaver implements iSaver {
         $this->connectionStatus = true;
         return true;
     }
-    
+
     public function SelectDatabase($name) {
-        return $this->SentQuery("use `".$name."`;");
+        return $this->SentQuery("use `" . $name . "`;");
     }
-    
+
+    private function ParseFromTeams($array) {
+        $sportsKindsNumber = 0;
+        $membersNumber = 0;
+        $teamsNumber = 0;
+        $membersToTeamsNumber = 0;
+        $sportsKindsId;
+        $memberList;
+
+        $this->participants = array();
+        foreach ($tables as $value) {
+            $this->participants[$value] = array();
+        }
+
+        foreach ($array as $value) {
+            $sportsKindsId = $this->SearchByName($this->participants["sports_kinds"], $value->sportsKind);
+            if ($sportsKindsId === FALSE) {
+                $sportsKindsNumber++;
+                $sportsKindsId = $sportsKindsNumber;
+                $this->participants["sports_kinds"][$sportsKindsNumber] = array("id" => $sportsKindsNumber, "name" => $value->sportsKind);
+            }
+
+            $teamsNumber++;
+            $this->participants["teams"][$teamsNumber] = array("id" => $teamsNumber, "name" => $value->name, "sports_kind_id" => $sportsKindsId, "motto" => $value->motto);
+
+            $memberList = $value->GetMembersList();
+            print_r($memberList);
+            foreach ($memberList as $memberName) {
+                $memberId = $this->SearchByName($this->participants["members"], $memberName);
+                if ($memberId === FALSE) {
+                    $member = $value->GetMemberBy($memberName, "name");
+                    $membersNumber++;
+                    $memberId = $membersNumber;
+                    echo "member = \n";
+                    print_r($member);
+                    $this->participants["members"][$membersNumber] = array("id" => $membersNumber, "name" => $member->name, "passport" => $member->passport);
+                }
+
+                $membersToTeamsNumber++;
+                $this->participants["members_teams"][$membersToTeamsNumber] = array("id" => $membersToTeamsNumber, "member_id" => $memberId, "team_id" => $teamsNumber);
+            }
+        }
+    }
+
+    private function SearchByName($array, $name) {
+        foreach ($array as $key => $value) {
+            if ($value["name"] == $name)
+                return $key;
+        }
+        return FALSE;
+    }
+
+    private function SaveTable($name, $values) {
+        $params = array();
+        $sql = "INSERT INTO `" . $name . "` (";
+        foreach ($values[0] as $key => $field) {
+            $sql .= $key . ",";
+        }
+        $sql[strlen($sql) - 1] = ") ";
+        $sql .= "VALUES";
+
+        foreach ($values as $field) {
+            $sql .= "(";
+            foreach ($field as $value) {
+                $sql .= "?,";
+                array_push($params, $value);
+            }
+            $sql[strlen($sql) - 1] = ")";
+            $sql .= ",";
+        }
+        $sql[strlen($sql) - 1] = ";";
+        return $this->SentQuery($sql, $params);
+    }
+
+    private function IncrementIds(&$values, $num) {
+        foreach ($values as $key => $value) {
+            $values[$key]["id"] += $num;
+        }
+        echo "num = " . $num;
+    }
+
+    private function GetMaxIdFromTable($name) {
+        $result = $this->SentQuery("SELECT MAX(id) FROM " . $name);
+        if ($result) {
+            $info = $this->lastStatement->fetch();
+            return $info["MAX(id)"];
+        }
+    }
+
     private function checkSupportingDBDriver($driverName) {
         $avaiableDrivers = PDO::getAvailableDrivers();
         if (in_array($driverName, $avaiableDrivers)) {
@@ -129,31 +177,31 @@ class DBSaver implements iSaver {
             return FALSE;
         }
     }
-    
-    private function DisconnectFromDB () {
+
+    private function DisconnectFromDB() {
         $this->pdo = NULL;
     }
-    
+
     private function CreateDatabaseIfNotExists($database) {
-        return $this->SentQuery("CREATE DATABASE IF NOT EXISTS `".$database."` CHARACTER SET utf8 COLLATE utf8_general_ci;");
+        return $this->SentQuery("CREATE DATABASE IF NOT EXISTS `" . $database . "` CHARACTER SET utf8 COLLATE utf8_general_ci;");
     }
-    
+
     private function CreateTables() {
-        
+
         $sql = "CREATE TABLE IF NOT EXISTS `sports_kinds` (
                   `id` int(11) NOT NULL AUTO_INCREMENT,
                   `name` varchar(50) NOT NULL,
                   PRIMARY KEY (`id`)
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
         $this->SentQuery($sql);
-        
+
         $sql = "CREATE TABLE IF NOT EXISTS `participants` (
                   `id` int(11) NOT NULL AUTO_INCREMENT,
                   `name` varchar(50) NOT NULL,
                   PRIMARY KEY (`id`)
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
         $this->SentQuery($sql);
-        
+
         $sql = "CREATE TABLE IF NOT EXISTS `teams` (
                   `id` int(11) NOT NULL AUTO_INCREMENT,
                   `name` varchar(50) NOT NULL,
@@ -163,7 +211,7 @@ class DBSaver implements iSaver {
                   CONSTRAINT `t2sk` FOREIGN KEY (`sports_kind_id`) REFERENCES `sports_kinds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
         $this->SentQuery($sql);
-        
+
         $sql = "CREATE TABLE IF NOT EXISTS `participants_teams` (
                   `id` int(11) NOT NULL AUTO_INCREMENT,
                   `participant_id` int(11) NOT NULL,
@@ -176,16 +224,16 @@ class DBSaver implements iSaver {
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
         $this->SentQuery($sql);
     }
-    
+
     private function DeleteTables() {
         foreach (array_reverse($this->tables) as $table) {
-            $sql = "DROP TABLE IF EXISTS `".$table."`;";
+            $sql = "DROP TABLE IF EXISTS `" . $table . "`;";
             if (!$this->SentQuery($sql)) {
                 trigger_error("Ошибка сохранения базы данных", E_USER_ERROR);
             }
         }
     }
-    
+
     private function SentQuery($sql, $input_params = NULL) {
         $sth = $this->pdo->prepare($sql);
         if ($input_params) {
@@ -201,7 +249,7 @@ class DBSaver implements iSaver {
         }
         return $result;
     }
-    
+
     function __destruct() {
         $this->DisconnectFromDB();
     }
